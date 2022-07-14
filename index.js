8768cfbb98e3a95f3f005685fc24dda0a0c6ebe2
Mayer Saint Germain <mayersaintgermain888@gmail.com>
2022-07-14 07:00:15 -0700

    javascript is hard

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+import Calculator from "./calculator.js";
 const numberButtons = document.querySelectorAll('[data-number]');
 const operationButtons = document.querySelectorAll('[data-operation]');
 const equalsButton = document.querySelector('[data-equals]');
@@ -5,3 +6,29 @@ const deleteButton = document.querySelector('[data-delete]');
 const allClearButton = document.querySelector('[data-all-clear');
 const previousOperandTextElement = document.querySelector('[data-previous-operand]');
 const currentOperandTextElement = document.querySelector('[data-current-operand]');
+
+const calculator = new Calculator(previousOperandTextElement,currentOperandTextElement);
+
+numberButtons.forEach(button=>{
+    button.addEventListener('click', () => {
+        calculator.appendNumber(button.innerText);
+        calculator.updateDisplay();
+    });
+});
+
+operationButtons.forEach(button=>{
+    button.addEventListener('click', () => {
+        calculator.chooseOperation(button.innerText);
+        calculator.updateDisplay();
+    });
+});
+
+equalsButton.addEventListener('click', button =>{
+    calculator.compute();
+    calculator.updateDisplay()
+})
+
+allClearButton.addEventListener('click', button =>{
+    calculator.clear();
+    calculator.updateDisplay()
+})
